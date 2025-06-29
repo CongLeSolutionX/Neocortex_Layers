d781afbf856fea370e21104f6bbf1386bf7bea93
Cong Le <longchike@gmail.com>
2025-06-29 00:33:51 -0700

    The setup

---
+++ Neocortex_Layers/Neocortex_LayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neocortex_LayersApp.swift
+//  Neocortex_Layers
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neocortex_LayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
